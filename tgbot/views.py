06406c0abd9b7ac81b7857b84ae9c3b49dcf4fd5
Nikita <shamaev.na@phystech.edu>
2021-10-25 22:35:21 +0300

    fix tgbot url

--- tgbot/views.py
+++ tgbot/views.py
@@ -3,6 +3,7 @@ import logging
 from django.views import View
 from django.http import JsonResponse
 
+from tgbot.dispatcher import process_telegram_event
 from tub.settings import DEBUG
 
 logger = logging.getLogger(__name__)
--- tub/urls.py
+++ tub/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('tgadmin/', admin.site.urls),
+    path('', include('tgbot.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
